cd26731e01c5bb2006aee3590624bed6d1b17686
daranha1@yahoo.ca <daranha1@yahoo.ca>
2020-11-18 05:40:30 -0500

    Second Commit - add gitignore pom and src

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private CartRepository cartRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+	
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		User user = new User();
+		user.setUsername(createUserRequest.getUsername());
+		log.info("Username successfully set : " + createUserRequest.getUsername());
+
+		Cart cart = new Cart();
+		cartRepository.save(cart);
+		user.setCart(cart);
+
+		if (createUserRequest.getPassword().length() < 7 ||
+				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			log.info("User Password Error : User not created : " + createUserRequest.getUsername());
+			return ResponseEntity.badRequest().build();
+		}
+
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		userRepository.save(user);
+		log.info("Success : User Created : " + createUserRequest.getUsername());
+		return ResponseEntity.ok(user);
+	}
+}
