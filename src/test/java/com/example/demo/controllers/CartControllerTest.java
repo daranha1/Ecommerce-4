20d807d54a4ae4f0a029109c13ef028b94c38870
daranha1@yahoo.ca <daranha1@yahoo.ca>
2020-11-20 04:22:18 -0500

    Sixth Commit - updated log and Test files

--- src/test/java/com/example/demo/controllers/CartControllerTest.java
+++ src/test/java/com/example/demo/controllers/CartControllerTest.java
@@ -10,6 +10,7 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.ModifyCartRequest;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
@@ -35,6 +36,55 @@ public class CartControllerTest {
         TestUtils.injectObjects(cartController, "userRepository", userRepos);
         TestUtils.injectObjects(cartController, "cartRepository", cartRepos);
         TestUtils.injectObjects(cartController, "itemRepository", itemRepos);
+    }
+
+    @Test
+    public void checkAddToCartNoUser() {
+        long itemId = 2L;
+        String username = "Stephen";
+        int quantity = 4;
+
+        ModifyCartRequest cartRequest = makeCartRequest(itemId, quantity, username);
+        ArrayList<Item> itemList = new ArrayList<Item>();
+
+        when(userRepos.findByUsername(username)).thenReturn(null);
+        when(itemRepos.findById(itemId)).thenReturn(null);
+
+        ResponseEntity<Cart> cartResponse = cartController.addToCart(cartRequest);
+        assertNotNull(cartResponse);
+        assertEquals(HttpStatus.NOT_FOUND, cartResponse.getStatusCode());
+    }
+
+    @Test
+    public void checkAddToCartNoItem() {
+        Cart cart1 = new Cart();
+
+        Long itemId = 2L;
+        String itemName = "Revlon Lipstick";
+        String itemDescription = "Luminous Red";
+        BigDecimal itemPrice = new BigDecimal("7.50");
+        Item item1 = makeItem(itemId, itemName, itemPrice, itemDescription);
+
+        long userId = 2L;
+        String username = "Angela";
+        String password = "testPass";
+
+        User user1 = makeUser(userId, username, password);
+        long cartId = 2L;
+        int quantity = 5;
+
+        ModifyCartRequest cartRequest = makeCartRequest(itemId, quantity, username);
+        cart1 = makeCart(cartId, null, user1);
+        user1.setCart(cart1);
+        cart1.setUser(user1);
+
+        when(userRepos.findByUsername(username)).thenReturn(user1);
+        when(itemRepos.findById(2L)).thenReturn(Optional.ofNullable(null));
+
+        ResponseEntity<Cart> cartResponse = cartController.addToCart(cartRequest);
+        assertNotNull(cartResponse);
+        assertEquals(HttpStatus.NOT_FOUND, cartResponse.getStatusCode());
+
     }
 
     @Test
@@ -111,5 +161,78 @@ public class CartControllerTest {
         Cart cart1 = cartRespEntity.getBody();
         assertEquals(200, cartRespEntity.getStatusCodeValue());
         assertEquals(1, cart1.getItems().size());
+    }
+
+    ////////// Test Unsuccessful Removal from Cart -- User does not exist //////////
+    @Test
+    public void checkRemovedFromCartNoUser() {
+        long itemId = 3L;
+        int quantity = 5;
+        String username = "Jane";
+
+        ModifyCartRequest cartRequest = makeCartRequest(itemId, quantity, username);
+        when(userRepos.findByUsername(username)).thenReturn(null);
+
+        ResponseEntity<Cart> cartResponse = cartController.removeFromCart(cartRequest);
+
+        assertNotNull(cartResponse);
+        assertEquals(HttpStatus.NOT_FOUND, cartResponse.getStatusCode());
+    }
+
+
+    ////////// Test Unsuccessful Removal from Cart - Null Item ////////
+    @Test
+    public void checkItemNotRemovedFromCart() {
+        Cart cart1 = new Cart();
+
+        Long itemId = 2L;
+        String itemName = "Revlon Lipstick";
+        BigDecimal itemPrice = new BigDecimal("7.50");
+        String itemDescription = "Luuminour Red";
+        Item item1 = makeItem(2L, itemName, itemPrice, itemDescription);
+
+        long userId = 2L;
+        String username = "Linda";
+        String password = "testpass";
+
+        User user1 = makeUser(userId, username, password);
+        user1.setCart(cart1);
+
+        when (userRepos.findByUsername(username)).thenReturn(user1);
+        when(itemRepos.findById(itemId)).thenReturn(Optional.ofNullable(null));
+
+    }
+
+    private Item makeItem (Long itemId, String itemName, BigDecimal itemPrice, String itemDescription) {
+        Item myItem = new Item();
+        myItem.setId(itemId);
+        myItem.setName(itemName);
+        myItem.setPrice(itemPrice);
+        myItem.setDescription(itemDescription);
+        return myItem;
+    }
+
+    private User makeUser(long userId, String username, String password) {
+        User user1 = new User();
+        user1.setId(userId);
+        user1.setUsername(username);
+        user1.setPassword(password);
+        return user1;
+    }
+
+    private Cart makeCart (long cartId, ArrayList<Item> items, User user) {
+        Cart myCart = new Cart();
+        myCart.setId(cartId);
+        myCart.setItems(items);
+        myCart.setUser(user);
+        return myCart;
+    }
+
+    private ModifyCartRequest makeCartRequest(long itemId, int quantity, String username) {
+        ModifyCartRequest cartReq = new ModifyCartRequest();
+        cartReq.setItemId(itemId);
+        cartReq.setQuantity(quantity);
+        cartReq.setUsername(username);
+        return cartReq;
     }
 }
--- src/test/java/com/example/demo/controllers/ItemControllerTest.java
+++ src/test/java/com/example/demo/controllers/ItemControllerTest.java
@@ -1,4 +1,109 @@
 package com.example.demo.controllers;
 
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ItemControllerTest {
+
+    private ItemController itemController;
+    private final ItemRepository itemRepo = mock (ItemRepository.class);
+
+    @Before
+    public void setUp(){
+        itemController = new ItemController();
+        TestUtils.injectObjects(itemController, "itemRepository", itemRepo);
+    }
+
+    ///////// Test 1 : Check Item List ///////////////////
+    @Test
+    public void checkGetItemsByName() {
+        String item1_Name = "Revlon Lipstick";
+        String item1_Description = "Luminous Red";
+        Long item1_Id = 2L;
+        BigDecimal item1_Price = new BigDecimal ("7.50");
+
+        String item2_Name = "MaxFactor Lipstick";
+        String item2_Description = "Coral";
+        Long item2_Id = 3L;
+        BigDecimal item2_Price = new BigDecimal ("10.50");
+
+        Item item1 = makeItem(item1_Name, item1_Description, item1_Id, item1_Price);
+        Item item2 = makeItem(item2_Name, item2_Description, item2_Id, item2_Price);
+
+        List<Item> itemList = new ArrayList<Item>();
+        itemList.add(item1);
+        itemList.add(item2);
+
+        when (itemRepo.findByName(item2_Name)).thenReturn(itemList);
+
+        ResponseEntity<List<Item>> itemResponse = itemController.getItemsByName(item2_Name);
+        assertNotNull(itemResponse);
+        assertEquals(HttpStatus.OK, itemResponse.getStatusCode());
+
+        List<Item> itemMatch = itemResponse.getBody();
+        assertNotNull(itemMatch);
+        assertEquals (item2_Id, itemMatch.get(1).getId());
+        assertEquals(item2_Name, itemMatch.get(1).getName());
+        assertEquals(item2_Description, itemMatch.get(1).getDescription());
+        assertEquals(item2_Price, itemMatch.get(1).getPrice());
+    }
+
+    //////// Test 2 : Check Item by Id //////////////////////////
+    @Test
+    public void checkGetItemById() {
+        String item1_Name = "Revlon Lipstick";
+        String item1_Description = "Luminous Red";
+        Long item1_Id = 2L;
+        BigDecimal item1_Price = new BigDecimal ("7.50");
+
+        Item item1 = makeItem(item1_Name, item1_Description, item1_Id, item1_Price);
+
+        when(itemRepo.findById(item1.getId())).thenReturn(Optional.of(item1));
+        ResponseEntity<Item> resp = itemController.getItemById(2L);
+
+        assertNotNull(resp);
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+
+        Item item = resp.getBody();
+        assertNotNull(item);
+        assertEquals(item1_Id, item.getId());
+        assertEquals(item1_Name, item.getName());
+        assertEquals(item1_Description, item.getDescription());
+        assertEquals(item1_Price, item.getPrice());
+    }
+
+    ////////// Test 3 : Check for empty list of items /////////////
+    @Test
+    public void checkGetItems() {
+        ResponseEntity<List<Item>> itemResponse = itemController.getItems();
+        assertNotNull(itemResponse);
+        assertEquals(HttpStatus.OK, itemResponse.getStatusCode());
+        assertNotNull(itemResponse.getBody());
+        assertEquals(0, itemResponse.getBody().size());
+    }
+
+    private Item makeItem(String itemName, String itemDescription, Long itemId, BigDecimal price) {
+        Item item = new Item();
+        item.setId(itemId);
+        item.setName(itemName);
+        item.setDescription(itemDescription);
+        item.setPrice(price);
+
+        return item;
+    }
 }
